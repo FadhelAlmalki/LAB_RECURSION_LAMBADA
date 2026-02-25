7db82954135bbf7d120ae3da2ae57934332e0c89
FadhelAlmalki <almalkifadhel@gmail.com>
2026-02-25 12:18:28 +0300

    Modified the way i built it using recursion

--- solution_1.py
+++ solution_1.py
@@ -1,4 +1,4 @@
-
+'''
 vowels = ['a', 'e', 'i', 'o', 'u']
 phrase = "I love python"
 
@@ -11,3 +11,17 @@ def count_vowels(phrase):
 
 vowel_count = count_vowels(phrase)
 print(f"The number of vowels in the phrase is: {vowel_count}")
+'''
+def count_vowels(phrase):
+    if not phrase:
+        return 0
+    first_char = phrase[0].lower()
+    if first_char in ['a', 'e', 'i', 'o', 'u']:
+        return 1 + count_vowels(phrase[1:])
+    else:
+        return count_vowels(phrase[1:])
+
+phrase = "I love python"
+vowel_count = count_vowels(phrase)
+print(f"The number of vowels in the phrase is: {vowel_count}")
+
